c20a2d10c467809166290bbfe17a80fb8d1c4237
ansgarvh <ansgar_tvh@web.de>
2026-08-29 14:52:21 +0200

    Bump PWA cache for v0.2.1

--- laufapp/app/static/sw.js
+++ laufapp/app/static/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='laufapp-v0.2.0';
+const CACHE='laufapp-v0.2.1';
 const staticUrls=()=>['./','styles.css','app.js','manifest.webmanifest','icon-192.png','assets/bugfix.css','assets/v020.css','assets/v020.js','assets/v020_science.css','assets/v020_science.js'].map(p=>new URL(p,self.registration.scope).toString());
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(staticUrls())).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
